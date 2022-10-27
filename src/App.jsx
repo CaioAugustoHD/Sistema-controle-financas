4f6ec3793ed568029b05fa72e425c3950422ff17
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-10-26 21:41:31 -0300

    Criado states

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header/Header'
 import { ConteinerSaldo } from './components/Saldo/ConteinerSaldo'
@@ -6,6 +6,13 @@ import { Form } from './components/Form/Form'
 
 export function App() {
 
+  const dados = localStorage.getItem("listaTransacoes");
+  const [listaTransacoes, setListaTransacoes] = useState (dados ? JSON.parse(dados) : []);
+
+  const [entrada, setEntrada] = useState(0);
+  const [saida, setSaida] = useState(0);
+  const [total, setTotal] = useState(0);
+  
   function registrarTransacao(novaTransacao){
     console.log(novaTransacao);
   }
